ebe80d0a3a801aeee1580420bc84fc3b80ec88c3
ngutierrezp <nicolas.gutierrez.p@usach.cl>
2020-05-11 20:17:39 -0400

    Fix: get and set current directory in main.r

--- main.r
+++ main.r
@@ -1,9 +1,16 @@
 
 dirstudio <- rstudioapi::getSourceEditorContext()$path
 
+dir <- getwd()
+
+setwd(dir)
+
 
 if(!exists("getAllData", mode="function")) source("scripts/genereteProccessed.R")
+if(!exists("showCorplot", mode="function")) source("scripts/corAnalysis.R")
 
 
 
 all.df <- getAllData()
+
+showCorplot(all.df)
