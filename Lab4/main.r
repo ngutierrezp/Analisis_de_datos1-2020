3c38b4c68f3176ebb48141d87259cac4ac7f3561
Nicolás Gutiérrez <nicolas.gutierrez.p@usach.cl>
2020-09-19 04:33:04 -0300

    FIX: comment encoding

--- Lab4/main.r
+++ Lab4/main.r
@@ -29,36 +29,49 @@ if(!exists("discretize.Data", mode="function")) source("scripts/discretize.R")
 if(!exists(c("filterItemSet","filterRules"), mode="function")) source("scripts/filter.R")
 
 #############################
-# LAB 4: ARBOLES DE DECISIÓN
+# LAB 4: ARBOLES DE DECISIÃ“N
 ############################
 
-# En este laboratorio se estudiará y aplicará la teoria de clasificaicón
-# sobre los arboles de decisión. Para esto se debe tener en cuenta que 
-# los arboles de decisión se pueden dividir en dos grupos
+# En este laboratorio se estudiarÃ¡ y aplicarÃ¡ la teoria de clasificaicÃ³n
+# sobre los arboles de decisiÃ³n. Para esto se debe tener en cuenta que 
+# los arboles de decisiÃ³n se pueden dividir en dos grupos
 
-# ARBOLES DE REGRESIÓN
-
-
-# ARBOLES DE CLASIFICACIÓN.
+# ARBOLES DE REGRESIÃ“N
 
 
-# Este último es el que se utilizará debido a que la variable de respuesta
+# ARBOLES DE CLASIFICACIÃ“N.
+
+
+# Este Ãºltimo es el que se utilizarÃ¡ debido a que la variable de respuesta
 # o clase es una variable categorica de dos niveles.
 
 
 ## Para utilizar estos arboles se necesita encontrar los nodos mas puros
-# u homogenios que nos permitan separar de buena forma la elección
+# u homogenios que nos permitan separar de buena forma la elecciÃ³n
 # de una clase.
 
 
 ### CROSS ENTROPY ###
-# Para encontrar los nodos más puros utilizaremos CROSS ENTROPY la cual
-# está implementada en el algoritmo c5.0 del paquete C50. La entropia nos dice
+# Para encontrar los nodos mÃ¡s puros utilizaremos CROSS ENTROPY la cual
+# estÃ¡ implementada en el algoritmo c5.0 del paquete C50. La entropia nos dice
 # que tan desordenado se encuentra un sistema. En el caso particular de arboles
-# nos dirá que tanta impureza tiene un nodo.  Si un nodo es puro, contiene 
-# únicamente observaciones de una clase, su entropía es cero. 
+# nos dirÃ¡ que tanta impureza tiene un nodo.  Si un nodo es puro, contiene 
+# Ãºnicamente observaciones de una clase, su entropÃ­a es cero. 
 # Por el contrario, si la frecuencia de cada clase es la misma, el valor de 
-# la entropía alcanza el valor máximo de 1.
+# la entropÃ­a alcanza el valor mÃ¡ximo de 1.
+
+
+
+### PROCESO DE CREACIÃ“N DE UN ARBOL ###
+# sin importan que metodo se haya utilizado para escoger el dividor de nodos
+# la creaciÃ³n de un arbol sigue los siguientes pasos:
+
+# 1. Para cada divisiÃ³n de un nodo se calcula el valor de la medida utilizada.
+# 2. Se pondera la pureza de la divisiÃ³n por el % de datos presente en la 
+# divisiÃ³n respecto al numero total de datos.
+# 3. se escoge la mejor divisiÃ³n respecto al valor calculado y se repite.
+
+
 
 
 
@@ -72,8 +85,8 @@ if(!exists(c("filterItemSet","filterRules"), mode="function")) source("scripts/f
 
 
 
-#     all.df :  Contiene toda la informaciï¿½n del dataset
-#               incluida la informaciï¿½n del lugar de donde
+#     all.df :  Contiene toda la informaciÃ¯Â¿Â½n del dataset
+#               incluida la informaciÃ¯Â¿Â½n del lugar de donde
 #               fueron obtenidos (cleve,swit,hung,va)
 all.df <- suppressWarnings(getAllData())
 
