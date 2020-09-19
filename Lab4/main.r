c1244aa04265ea92d5423e965d622cdf81dfff35
Nicolás Gutiérrez <nicolas.gutierrez.p@usach.cl>
2020-09-19 03:44:37 -0300

    ADD: coment for test

--- Lab4/main.r
+++ Lab4/main.r
@@ -33,7 +33,32 @@ if(!exists(c("filterItemSet","filterRules"), mode="function")) source("scripts/f
 ############################
 
 # En este laboratorio se estudiará y aplicará la teoria de clasificaicón
-# sobre los arboles de decisión. Para esto 
+# sobre los arboles de decisión. Para esto se debe tener en cuenta que 
+# los arboles de decisión se pueden dividir en dos grupos
+
+# ARBOLES DE REGRESIÓN
+
+
+# ARBOLES DE CLASIFICACIÓN.
+
+
+# Este último es el que se utilizará debido a que la variable de respuesta
+# o clase es una variable categorica de dos niveles.
+
+
+## Para utilizar estos arboles se necesita encontrar los nodos mas puros
+# u homogenios que nos permitan separar de buena forma la elección
+# de una clase.
+
+
+### CROSS ENTROPY ###
+# Para encontrar los nodos más puros utilizaremos CROSS ENTROPY la cual
+# está implementada en el algoritmo c5.0 del paquete C50. La entropia nos dice
+# que tan desordenado se encuentra un sistema. En el caso particular de arboles
+# nos dirá que tanta impureza tiene un nodo.  Si un nodo es puro, contiene 
+# únicamente observaciones de una clase, su entropía es cero. 
+# Por el contrario, si la frecuencia de cada clase es la misma, el valor de 
+# la entropía alcanza el valor máximo de 1.
 
 
 
