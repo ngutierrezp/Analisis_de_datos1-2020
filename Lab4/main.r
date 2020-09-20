7f3fd3d2942a589e01f1c252dbe31ecc6341650a
Nicolas <lizwert.g@gmail.com>
2020-09-20 07:46:22 -0300

    FEAT: lab4 finished with comments

--- Lab4/main.r
+++ Lab4/main.r
@@ -6,12 +6,15 @@
 #install.packages("C50")
 #install.packages("caret")
 #install.packages("tree")
+#install.packages("ggpubr")
 
 
 library(tidyverse)
 library(C50)
-library(caret)
 library(tree)
+library(caret)
+library(ggpubr)
+
 
 
 ###################################
@@ -233,7 +236,7 @@ predic.arbolC50 <- predict(arbol.c50, test.df[,-11], type = "class")
 pre.matriz <- table(test.df$disease, predic.arbolC50)
 
 # matriz de confusión
-matrix <- confusionMatrix(pre.matriz)
+matrix <- caret::confusionMatrix(pre.matriz)
 
 
 
@@ -245,7 +248,269 @@ matrix <- confusionMatrix(pre.matriz)
 # El acurrancy es cerca del 81%, si bien no es un valor bajo
 # tampoco es un valor muy alto como los obtenidos en el modelo de regresión.
 
-# 
+# Otro punto importante a destacar es la significancia que tuvo las pruebas
+#  de binom.test y mcnemar.test las cuales terminan siendo significativas (< 0.05)
+# lo cual primero nos dice que los resultados del modelo fueron mucho mas 
+# precisos que el NIR.
 
+# Respecto al p-valor de mcnemar.test nos dice que no existen cambios
+# entre el conjunto de test y lo predicho por el modelo, es decir, el acurrancy
+# obtenido por el modelo no varia por el set de prueba lo cual para este caso es 
+# bueno.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################
+# PARTE 2: Mejoramiento del Modelo
+####################################
+
+
+# Como estamos dentro de un problema que implica trabajar con pacientes con 
+# posibles dolores al corazón, es importante realizar el mejor estudio posible
+# debido a que predecir en un 81% de forma correcta si existe o no una enfermedad
+# implica que en el peor de los casos el 19% de los pacientes podria morir si 
+# el clasificador falla. Es por esto que necesitamos mejorarlo y tratar de 
+# llegar un predictor óptimo.
+
+
+
+
+
+
+
+
+#   Boosting
+# ------------------------
+
+
+# Primeramente al modelo antes obtenido le aplicaremos boosting para obtener
+# un modelo del tipo ensemble.
+
+# indicamos que queremos 8 iteraciones de boosting
+boost.tree.c50 <- C5.0(disease ~ ., data = train.df, trials=14)
+
+
+
+
+### 1.1  Generación de matriz de confución
+########
+
+# Generación de datos de prueba respecto al set de entrenamiento.
+predic.boost.tree <- predict(boost.tree.c50, test.df[,-11], type = "class")
+
+# pre-matriz con los datos tabulados
+pre.boost.matriz <- table(test.df$disease, predic.boost.tree)
+
+# matriz de confusión
+boost.matrix <- caret::confusionMatrix(pre.boost.matriz)
+
+
+### 1.2 Conclusión Boosting :
+#######
+
+# Como se puede ver en los resultados de la matriz de confusión, se obtuvo una
+# mejora en el accurancy además de la sensibilidad y especificidad lo cual
+# son buenos indicadores. Respecto al P-value, nos sigue diciendo que 
+# los datos ajustado son significativamente mayores al NIR. Respecto al Test
+# de mcNemar, el p-valor tiende a ser mayor lo que nos dice que posiblemente
+# con este nuevo modelo generado, exista un sobreajuste. 
+
+
+# En resumen, el modelo generado por boosting es mucho mejor pero 
+# posiblemente tenga algun grado de sobreajuste por lo que hay que tomar en 
+# cuenta esto y ver si se puede resolver.
+
+
+
+
+
+
+
+
+#   variables influyentes
+# ------------------------
+
+# Parte importante es analizar las variables más utilizadas y significativas en
+# el modelo creado.
+
+# Para ver que variables son las más influyentes en la predicción utilizaremos
+# las metricas de :
+
+# USAGE: Cuantos datos pasan por las variables. 
+# SPLIT: Cuantas veces el predictor se divide.
+
+
+# Utilizaremós el mejor modelo que tenemos por el momento que es el de boosting
+
+## Metrica USAGE
+imp_vars_usag <- C5imp(boost.tree.c50, metric = "usage")
+imp_vars_usag <- imp_vars_usag %>%
+  rownames_to_column(var = "predictor")
+
+## METRICA SPLITS
+imp_vars_spli <- C5imp(boost.tree.c50, metric = "splits")
+imp_vars_spli <- imp_vars_spli  %>%
+  rownames_to_column(var = "predictor")
+
+
+
+# Utilicemos un grafico para mostrar el comportamiento de las variables:
+
+plot.usage <- ggplot(data = imp_vars_usag, aes(x = reorder(predictor, Overall),
+                                           y = Overall, fill = Overall)) +
+  labs(x = "predictor", title = "usage") +
+  geom_col() +
+  coord_flip() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+plot.split <- ggplot(data = imp_vars_spli, aes(x = reorder(predictor, Overall),
+                                            y = Overall, fill = Overall)) +
+  labs(x = "predictor", title = "splits") +
+  geom_col() +
+  coord_flip() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+  
+
+metric.plot <-ggarrange(plot.usage, plot.split)
+
+
+
+
+
+
+### 2.1 Generación de Árbol con Winnowing 
+#######
+
+
+# Este algormitmo nos dirá que tan importantes son las variables.
+# Volveremos a general el arbol de boosting pero ahora estudiando las 
+# variables.
+
+boost.tree.c50.vars <- C5.0(disease ~ ., data = train.df, trials=14,
+                            control= C5.0Control(winnow = T))
+
+
+
+
+### 2.2 Conclusión Variables importantes :
+#######
+
+# Según las metricas, la variable con mayor importancia es la edad luego, el tipo
+# de dolor de pecho que sienta una persona (cp) y el sexo que tenga esa persona.
+
+# Pero la variable que tiende a importar menos es Chol, es decir, si una
+# persona tiene el colesteror estable o no. 
+
+# Si se analiza la variable Chol, se puede ver que esta posee una cantidad 
+# importante de NA´s (202). Podemos ver que más del 89% de los pacientes con
+# esta variable presentan anomalias en el colesterol. Recordemos que un 
+# colesterol normal es menor a 200 mg/dL. Entonces perfectamente podemos 
+# sacar esta variable ya que no nos da información importante
+# ya que la gran mayoria de los pacientes tiene un colesterol alto.
+
+# Con el algoritmo de Winnowing 3 Variables fueron eliminadas dejando
+# las más importantes como : age, sex, cp, exang, fbs, thalach, restecg.
+#                            (1)  (2) (3)   (9)   (6)      (8)    (7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################
+# PARTE 3: Modelo Final
+####################################
+
+# Con el estudio realizado a los modelos anteriores ya podemos generar
+# el mejor Álbol de desición posible.
+
+# DF con las variables importantes.
+imp.var.df <- factor.all.df[,c(1,2,3,6,7,8,9,11)]
+
+
+
+
+### Generamos unos nuevos set de entrenamiento y prueba.
+
+set.seed(985426)
+
+
+ntrain.imp <- nrow(imp.var.df) * 0.7
+
+index_train.imp<-sample(1:nrow(imp.var.df),size = ntrain.imp)
+
+imp.train.df<-imp.var.df[index_train.imp,]
+
+imp.test.df<-imp.var.df[-index_train.imp,]
+
+
+
+# Verificamos que la diferencia entre los set sea menor a un 5%.
+
+imp.diference.df <- equal.train.test(imp.train.df,imp.test.df)
+
+# Como es cerca del 3% podemos suponer que ambos set estan bien distribuidos.
+
+
+
+### Generamos el arbol.
+imp.boost.tree <- C5.0(disease ~ ., data = imp.train.df, trials=18)
+
+
+### Generamos la matriz de confusión para comprobar metricas
+
+imp.predic.boost.tree <- predict(imp.boost.tree, imp.test.df[,-8], type = "class")
+
+# pre-matriz con los datos tabulados
+imp.pre.boost.matriz <- table(imp.test.df$disease, imp.predic.boost.tree)
+
+# matriz de confusión
+imp.boost.matrix <- caret::confusionMatrix(imp.pre.boost.matriz)
+
+plot(imp.boost.tree)
+
+###
+## Analisis
+
+# El árbol obtenido producto de la fase de mejoramiento realmente resulto ser
+# mucho mejor a los anteriores debido a que si bien el acurrancy no mejoró mucho
+# respecto al arbol anterior, un 1% puede dar la diferencia y de todas formas
+# ayuda de sobremanera. La exactitud del arbol sigue siendo significativa
+# respecto al NIR debido a que el p-valor es de 2*e^(-16). 
+# Respecto al p-Valor de Macnemar, si bien este no es significativo ha bajado
+# y se encuentra casi al limite lo que da indición que existe un sobreajuste
+# pero este es leve por lo que si bien afecta al modelo, no lo hace 
+# con tanta fuerza. Recordemos que este último test no es del todo preciso ya
+# que depende de los datos por lo que sus valors pueden variar con otros.
+
+
+# En sistesis se ha creado un buen modelo el que hay que comparar con 
+# otros modelo obtenidos por otros metodos. 
 
 
